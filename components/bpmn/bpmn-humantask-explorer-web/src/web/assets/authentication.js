f32cad20b18409b3bac5ba79fd95ec47654e33a1
wso2 <warudanu@gmail.com>
2014-07-29 14:24:17 +0530

    use carbon local ip for support port offsets

--- components/bpmn/bpmn-humantask-explorer-web/src/web/assets/authentication.js
+++ components/bpmn/bpmn-humantask-explorer-web/src/web/assets/authentication.js
@@ -1,5 +1,9 @@
 function authenticate(username, password){
-	var carbon = require('carbon');
-	srv = new carbon.server.Server({url: "https://localhost:9443/admin"});
+   var 	carbon = require('carbon'),
+	process = require('process'),
+	localIP = process.getProperty('carbon.local.ip'),
+   	httpsPort = process.getProperty('mgt.transport.https.port'),
+	httpUrl = "https://"+localIP+":"+httpsPort,
+	srv = new carbon.server.Server({url: httpUrl});
 	return srv.authenticate(username, password);
 }
