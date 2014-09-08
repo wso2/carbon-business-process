7ef69d5fd4ceef370f57660b8697f7ee2fbdcf69
amalhub <amalg@wso2.com>
2014-09-08 21:39:30 +0530

    Adding user groups filter to tasks inbox

--- components/bpmn/bpmn-explorer-web/src/web/assets/authentication.js
+++ components/bpmn/bpmn-explorer-web/src/web/assets/authentication.js
@@ -7,3 +7,17 @@ function authenticate(username, password){
 	srv = new carbon.server.Server({url: httpsUrl});
 	return srv.authenticate(username, password);
 }
+
+function getRoles(username, password){
+   var 	carbon = require('carbon'),
+	process = require('process'),
+	localIP = process.getProperty('carbon.local.ip'),
+   	httpsPort = process.getProperty('mgt.transport.https.port'),
+	httpsUrl = "https://"+localIP+":"+httpsPort,
+	srv = new carbon.server.Server({url: httpsUrl}),
+	tenantId = -1234,
+	userManager = new carbon.user.UserManager(srv, tenantId),
+	user = new carbon.user.User(userManager, username),
+	roles = user.getRoles();
+	return roles;
+}
