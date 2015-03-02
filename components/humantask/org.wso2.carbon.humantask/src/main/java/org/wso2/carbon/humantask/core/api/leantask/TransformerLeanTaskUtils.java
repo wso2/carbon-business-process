0ff982ebfcc1fbdde63c1dfecd16b5217f7b4c8a
Subasinghe <sudharmap@wso2.com>
2015-03-02 11:24:17 +0000

    added all files

---
+++ components/humantask/org.wso2.carbon.humantask/src/main/java/org/wso2/carbon/humantask/core/api/leantask/TransformerLeanTaskUtils.java
@@ -0,0 +1,52 @@
+package org.wso2.carbon.humantask.core.api.leantask;
+
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axis2.databinding.ADBException;
+import org.apache.xmlbeans.XmlException;
+import org.wso2.carbon.humantask.*;
+import org.wso2.carbon.humantask.client.api.TLeanTask;
+import org.wso2.carbon.humantask.core.utils.DOMUtils;
+import org.wso2.carbon.humantask.impl.LeanTaskDocumentImpl;
+import org.wso2.carbon.humantask.impl.TLeanTaskImpl;
+
+import javax.xml.namespace.QName;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: suba
+ * Date: 2/25/15
+ * Time: 5:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TransformerLeanTaskUtils {
+
+    public static LeanTaskDocument transformLeanTask(TLeanTask adbLeantask) throws ADBException, XmlException {
+
+        org.wso2.carbon.humantask.TLeanTask xmlbLeantask = org.wso2.carbon.humantask.TLeanTask.Factory.newInstance();
+
+
+        QName ns = new QName("http://docs.oasis-open.org/ns/bpel4people/ws-humantask/200803", "taskDefinition");
+
+
+        OMElement taskDef = adbLeantask.getOMElement(ns, OMAbstractFactory.getOMFactory());
+        xmlbLeantask = org.wso2.carbon.humantask.TLeanTask.Factory.parse(taskDef.toString());
+        //optional
+//
+//        xmlbLeantask.setPriority(adbLeantask.getPriority());
+//        xmlbLeantask.setPeopleAssignments((TPeopleAssignments) adbLeantask.getPeopleAssignments());
+//        xmlbLeantask.setDelegation((TDelegation) adbLeantask.getDelegation());
+//        xmlbLeantask.setPresentationElements((TPresentationElements) adbLeantask.getPresentationElements());
+//        xmlbLeantask.setRenderings((TRenderings) adbLeantask.getRenderings());
+//        xmlbLeantask.setOutcome((TQuery) adbLeantask.getOutcome());
+//        xmlbLeantask.setSearchBy((TExpression) adbLeantask.getSearchBy());
+//        xmlbLeantask.setDeadlines((TDeadlines) adbLeantask.getDeadlines());
+
+        LeanTaskDocument document = LeanTaskDocument.Factory.newInstance();
+        document.setLeanTask(xmlbLeantask);
+
+        return document;
+    }
+
+
+}
