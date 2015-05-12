27f64c54b55a821618eb7a00b1116d025399823e
Himasha Guruge <himashag@wso2.com>
2015-05-12 13:30:12 +0530

    Unused import removed

--- components/bpmn/org.wso2.carbon.bpmn/src/main/java/org/wso2/carbon/bpmn/core/mgt/services/BPMNUploader.java
+++ components/bpmn/org.wso2.carbon.bpmn/src/main/java/org/wso2/carbon/bpmn/core/mgt/services/BPMNUploader.java
@@ -32,7 +32,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.jar.JarFile;
 
 /**
  * BPMN package uploader admin service
