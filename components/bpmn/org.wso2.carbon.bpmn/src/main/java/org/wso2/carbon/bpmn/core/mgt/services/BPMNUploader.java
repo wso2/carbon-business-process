5d5e8c706473cc59f1e1dc0c7360ca5f9b7c3832
Himasha Guruge <himashag@wso2.com>
2015-05-12 13:24:02 +0530

    Removing duplicate log of HumanTaskServer

--- components/bpmn/org.wso2.carbon.bpmn/src/main/java/org/wso2/carbon/bpmn/core/mgt/services/BPMNUploader.java
+++ components/bpmn/org.wso2.carbon.bpmn/src/main/java/org/wso2/carbon/bpmn/core/mgt/services/BPMNUploader.java
@@ -32,6 +32,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.jar.JarFile;
 
 /**
  * BPMN package uploader admin service
