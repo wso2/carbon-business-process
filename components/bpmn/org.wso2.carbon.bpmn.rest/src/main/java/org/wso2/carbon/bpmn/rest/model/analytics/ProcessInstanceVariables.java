c87a37e1ab3a543bc19894a81a313185c2934fe3
Samitha Chathuranga <rmschathuranga@gmail.com>
2016-02-19 09:48:20 +0530

    Added rest API to get  process instance variables-not finished

---
+++ components/bpmn/org.wso2.carbon.bpmn.rest/src/main/java/org/wso2/carbon/bpmn/rest/model/analytics/ProcessInstanceVariables.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.bpmn.rest.model.analytics;
+
+/**
+ * Created by samithac on 17/2/16.
+ */
+public class ProcessInstanceVariables {
+}
---
+++ components/bpmn/org.wso2.carbon.bpmn.rest/src/main/java/org/wso2/carbon/bpmn/rest/service/analytics/ProcessInstanceVariableSevice.java
@@ -0,0 +1,7 @@
+package org.wso2.carbon.bpmn.rest.service.analytics;
+
+/**
+ * Created by samithac on 17/2/16.
+ */
+public class ProcessInstanceVariableSevice {
+}
