5df913ea65630468b6dd22a0b94c86640480ade8
rmschathuranga@gmail.com <rmschathuranga@gmail.com>
2016-03-18 14:15:03 +0530

    Added rest API class/function to get Process ID and process variables from PC side

--- components/bpmn/org.wso2.carbon.bpmn.rest/src/main/java/org/wso2/carbon/bpmn/rest/service/analytics/PublishProcessVariablesService.java
+++ components/bpmn/org.wso2.carbon.bpmn.rest/src/main/java/org/wso2/carbon/bpmn/rest/service/analytics/PublishProcessVariablesService.java
@@ -4,8 +4,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONString;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Map;
 
@@ -13,15 +13,49 @@ import java.util.Map;
  * Created by samithac on 17/3/16.
  */
 
+
 @Path("/publish-process-variables")
 public class PublishProcessVariablesService {
     private static final Log log = LogFactory.getLog(PublishProcessVariablesService.class);
 
-    @Path("/{processId}/{processVariables}")
-    Response publishProcessVariables(@PathParam("processId") String processId,@PathParam("processVariables") JSONString processVariablesJson){
+    @PUT
+    @Path("/{processId}")///{processVariables}")
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public void publishProcessVariables(@PathParam("processId") String processId,@QueryParam("processVariables") String processVariablesJson){//},@PathParam("processVariables") JSONString processVariablesJson){
+        log.info("Process ID:"+processId+"\nvariables:"+processVariablesJson);//+processVariablesJson);
+        log.info("sssssssssss");
+        //https://192.168.43.51:9443/bpmn/analytics/publish-process-variables/ffff?processVariables=aaaaaaabbbbbbbb
+        //https://192.168.43.51:9443/bpmn/analytics/publish-process-variables/ffff?processVariables={"dddd":"FFF"}
 
-        log.info("Process ID:"+processId+"\nvariables:"+processVariablesJson);
-
-        return null;
     }
 }
+
+//////////
+/*
+@Path("/historic-activity-instances")
+public class HistoricActivitiInstanceService extends BaseHistoricActivitiInstanceService {
+
+    @GET
+    @Path("/")
+    @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
+    public Response getHistoricActivityInstances(@Context UriInfo uriInfo) {
+
+        Map<String, String> allRequestParams = new HashMap<>();
+
+        for (String property:allPropertiesList){
+            String value= uriInfo.getQueryParameters().getFirst(property);
+
+            if(value != null){
+                allRequestParams.put(property, value);
+            }
+        }
+
+        HistoricActivityInstanceQueryRequest query = getHistoricActivityInstanceQueryRequest(uriInfo, allRequestParams);
+
+        DataResponse dataResponse = getQueryResponse(query, allRequestParams, uriInfo);
+        return Response.ok().entity(dataResponse).build();
+    }
+
+
+}
+*/
