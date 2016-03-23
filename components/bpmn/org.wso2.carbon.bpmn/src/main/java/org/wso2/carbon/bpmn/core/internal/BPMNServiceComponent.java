6003e7d4bbdc34e84b19cd6f55ab7accf81193d4
nandika <nandika@wso2.com>
2016-03-23 14:26:08 +0530

    Updated poms to move to c5

--- components/bpmn/org.wso2.carbon.bpmn/src/main/java/org/wso2/carbon/bpmn/core/internal/BPMNServiceComponent.java
+++ components/bpmn/org.wso2.carbon.bpmn/src/main/java/org/wso2/carbon/bpmn/core/internal/BPMNServiceComponent.java
@@ -1,115 +1,101 @@
 /**
- *  Copyright (c) 2014-2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright (c) 2014-2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.wso2.carbon.bpmn.core.internal;
 
-//import org.osgi.framework.BundleContext;
 
+import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
 
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+import org.osgi.service.jndi.JNDIContextManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-//import org.osgi.service.component.annotations.Reference;
-//import org.osgi.service.component.annotations.ReferenceCardinality;
-//import org.osgi.service.component.annotations.ReferencePolicy;
-//import org.osgi.service.jndi.JNDIContextManager;
 import org.wso2.carbon.bpmn.core.ActivitiEngineBuilder;
+import org.wso2.carbon.bpmn.core.BPMNEngineService;
 import org.wso2.carbon.bpmn.core.BPMNServerHolder;
-import org.wso2.carbon.bpmn.core.deployment.BPMNDeployer;
-//import org.wso2.carbon.kernel.deployment.Artifact;
-//import org.wso2.carbon.kernel.deployment.ArtifactType;
-//import org.wso2.carbon.kernel.startupresolver.RequiredCapabilityListener;
-
-//import java.io.File;
-
-//import org.wso2.carbon.bpmn.core.BPMNEngineService;
-//import org.wso2.carbon.bpmn.core.db.*;
-//import org.wso2.carbon.bpmn.core.deployment.BPMNDeployer;
-//import org.wso2.carbon.bpmn.core.exception.BPMNMetaDataTableCreationException;
-//import org.wso2.carbon.bpmn.core.exception.DatabaseConfigurationException;
-//import org.activiti.engine.RepositoryService;
-//import org.activiti.engine.ProcessEngine;
-//import org.activiti.engine.repository.ProcessDefinition;
-
-//import java.io.File;
-//import java.util.List;
-
-//import org.wso2.carbon.kernel.deployment.Artifact;
-//import org.wso2.carbon.kernel.deployment.ArtifactType;
-//import org.wso2.carbon.kernel.startupresolver.RequiredCapabilityListener;
+import org.wso2.carbon.bpmn.core.db.DataSourceHandler;
+import org.wso2.carbon.kernel.startupresolver.RequiredCapabilityListener;
 
 /**
  *
  */
 
-public class BPMNServiceComponent {
+@Component(
+        name = "org.wso2.carbon.bpmn.core.internal.BPMNServiceComponent",
+        immediate = true
+)
+public class BPMNServiceComponent implements RequiredCapabilityListener {
 
     private static final Logger log = LoggerFactory.getLogger(BPMNServiceComponent.class);
-    //@Reference(
-    //name = "org.wso2.carbon.datasource.jndi",
-    //service = JNDIContextManager.class,
-    //cardinality = ReferenceCardinality.AT_LEAST_ONE,
-    //policy = ReferencePolicy.DYNAMIC,
-    //unbind = "deactivate"
-    //)
-    //@Reference(
-    //name = "org.wso2.carbon.kernel.datasource.core.
-    // internal.DataSourceListenerComponent",
-    //service = RequiredCapabilityListener.class,
-    //cardinality = ReferenceCardinality.AT_LEAST_ONE,
-    //policy = ReferencePolicy.DYNAMIC,
-    //unbind = "deactivate"
-    //)
 
+
+    @Reference(
+            name = "org.wso2.carbon.datasource.jndi",
+            service = JNDIContextManager.class,
+            cardinality = ReferenceCardinality.AT_LEAST_ONE,
+            policy = ReferencePolicy.DYNAMIC,
+            unbind = "unRegisterJNDIContext"
+    )
+
+    public void registerJNDIContext(JNDIContextManager contextManager) {
+        log.info("Registering JNDI Context");
+
+    }
+
+    public void unRegisterJNDIContext(JNDIContextManager contextManager) {
+        log.info("Unregister JNDI Context");
+    }
+
+    @Activate
     protected void activate(ComponentContext ctxt) {
-        log.error("Initializing the BPMN core component...");
+        log.info("Initializing the BPMN core component...");
+
         try {
-            // BundleContext bundleContext = ctxt.getBundleContext();
+            BundleContext bundleContext = ctxt.getBundleContext();
             BPMNServerHolder holder = BPMNServerHolder.getInstance();
-            ActivitiEngineBuilder activitiEngineBuilder = new ActivitiEngineBuilder();
-            holder.setEngine(activitiEngineBuilder.buildEngine());
-
-            BPMNEngineServiceImpl bpmnEngineService = new BPMNEngineServiceImpl();
-            bpmnEngineService.setProcessEngine(ActivitiEngineBuilder.getProcessEngine());
-
-            BPMNDeployer customDeployer = new BPMNDeployer();
-            customDeployer.init();
-            //File ab = new File(
-                   // "/Users/himasha/Desktop/Latest/new/wso2bps-3.5.1/repository/samples/bpmn/HelloWorld.bar");
-            //Artifact artifact = new Artifact(ab);
-            //ArtifactType artifactType = new ArtifactType<>("bar");
-            //artifact.setKey("HelloWorld.bar");
-           // artifact.setType(artifactType);
-           // customDeployer.deploy(artifact);
-           // log.error("Deployed in c5");
-
-            // DataSourceHandler dataSourceHandler = new DataSourceHandler();
-            //dataSourceHandler.initDataSource(activitiEngineBuilder.getDataSourceJndiName());
-            // dataSourceHandler.closeDataSource();
+//            ActivitiEngineBuilder activitiEngineBuilder = new ActivitiEngineBuilder();
+//            holder.setEngine(activitiEngineBuilder.buildEngine());
+//
+//            BPMNEngineServiceImpl bpmnEngineService = new BPMNEngineServiceImpl();
+//            bpmnEngineService.setProcessEngine(activitiEngineBuilder.getProcessEngine());
+//            bundleContext.registerService(BPMNEngineService.class.getName(), bpmnEngineService, null);
+//            DataSourceHandler dataSourceHandler = new DataSourceHandler();
+//            dataSourceHandler.initDataSource(activitiEngineBuilder.getDataSourceJndiName());
+//            dataSourceHandler.closeDataSource();
 
         } catch (Throwable e) {
             log.error("Failed to initialize the BPMN core component.", e);
         }
     }
-
+    @Deactivate
     protected void deactivate(ComponentContext ctxt) {
         log.info("Stopping the BPMN core component...");
         //ProcessEngines.destroy();
     }
 
+    @Override
+    public void onAllRequiredCapabilitiesAvailable() {
+        log.info("OnAllRequiredCapabilitiesAvailable");
+    }
 }
 
