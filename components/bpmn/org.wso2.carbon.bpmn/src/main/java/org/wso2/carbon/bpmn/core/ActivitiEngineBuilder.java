6a7354adaaeb4c64d6694217a884298c1bddedd4
nandika <nandika@wso2.com>
2016-03-25 18:40:19 +0530

    Fixing bpmn component initliazation issues

--- components/bpmn/org.wso2.carbon.bpmn/src/main/java/org/wso2/carbon/bpmn/core/ActivitiEngineBuilder.java
+++ components/bpmn/org.wso2.carbon.bpmn/src/main/java/org/wso2/carbon/bpmn/core/ActivitiEngineBuilder.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright (c) 2014 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright (c) 2014 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.wso2.carbon.bpmn.core;
@@ -21,7 +21,8 @@ import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.utils.CarbonUtils;
+import org.wso2.carbon.kernel.utils.Utils;
+
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -35,7 +36,7 @@ public class ActivitiEngineBuilder {
     private static final Log log = LogFactory.getLog(ActivitiEngineBuilder.class);
 
     private String dataSourceJndiName = null;
-    private static  ProcessEngine processEngine = null;
+    private static ProcessEngine processEngine = null;
 
 
      /* Instantiates the engine. Builds the state of the engine
@@ -47,23 +48,23 @@ public class ActivitiEngineBuilder {
     public ProcessEngine buildEngine() throws BPSFault {
 
         try {
-            String carbonConfigDirPath = CarbonUtils.getCarbonConfigDirPath();
+            String carbonConfigDirPath = Utils.getCarbonConfigHome().toAbsolutePath().toString();
             String activitiConfigPath = carbonConfigDirPath + File.separator +
-                                        BPMNConstants.ACTIVITI_CONFIGURATION_FILE_NAME;
+                    BPMNConstants.ACTIVITI_CONFIGURATION_FILE_NAME;
             File activitiConfigFile = new File(activitiConfigPath);
             ProcessEngineConfigurationImpl processEngineConfigurationImpl =
                     (ProcessEngineConfigurationImpl) ProcessEngineConfiguration
                             .createProcessEngineConfigurationFromInputStream(
                                     new FileInputStream(activitiConfigFile));
             // we have to build the process engine first to initialize session factories.
-          processEngine = processEngineConfigurationImpl.buildProcessEngine();
+            processEngine = processEngineConfigurationImpl.buildProcessEngine();
             //  processEngineConfigurationImpl.getSessionFactories().put(UserIdentityManager.class,
             //   new BPSUserManagerFactory());
             //  processEngineConfigurationImpl.getSessionFactories().put(GroupIdentityManager.class,
             //new BPSGroupManagerFactory());
 
             dataSourceJndiName = processEngineConfigurationImpl.getProcessEngineConfiguration()
-                                                               .getDataSourceJndiName();
+                    .getDataSourceJndiName();
 
         } catch (FileNotFoundException e) {
             String msg =
