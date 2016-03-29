c34fd92c3f885ca356644455a05c34d85c490510
Hasitha Aravinda <mail.hasitha27@gmail.com>
2016-03-29 20:53:39 +0530

    fixing findbug SIC_INNER_SHOULD_BE_STATIC_ANON

--- components/bpmn/org.wso2.carbon.bpmn/src/main/java/org/wso2/carbon/bpmn/core/mgt/dao/ActivitiDAO.java
+++ components/bpmn/org.wso2.carbon.bpmn/src/main/java/org/wso2/carbon/bpmn/core/mgt/dao/ActivitiDAO.java
@@ -1,17 +1,17 @@
 /**
- *  Copyright (c) 2014-2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.wso2.carbon.bpmn.core.mgt.dao;
@@ -44,19 +44,14 @@ public class ActivitiDAO {
     }
 
     /**
-     * invokes the DeploymentMapper.selectMetaData for a given tenant id and package name
+     * invokes the DeploymentMapper.selectMetaData for a given tenant id and package name.
      *
      * @param bpmnPackageName package name
      * @return DeploymentMetaDataModel object
      */
     public DeploymentMetaDataModel selectDeploymentModel(final String bpmnPackageName) {
         CustomSqlExecution<DeploymentMapper, DeploymentMetaDataModel> customSqlExecution =
-                new AbstractCustomSqlExecution<DeploymentMapper, DeploymentMetaDataModel>(
-                        DeploymentMapper.class) {
-                    public DeploymentMetaDataModel execute(DeploymentMapper deploymentMapper) {
-                        return deploymentMapper.selectMetaData(bpmnPackageName);
-                    }
-                };
+                new GetDeploymentMetaDataModelSqlExecution(bpmnPackageName);
 
         DeploymentMetaDataModel deploymentMetaDataModel =
                 managementService.executeCustomSql(customSqlExecution);
@@ -64,7 +59,7 @@ public class ActivitiDAO {
 
             if (deploymentMetaDataModel != null) {
                 log.debug("DeploymentDataModel exists when selecting models=" +
-                          deploymentMetaDataModel.getId());
+                        deploymentMetaDataModel.getId());
             } else {
                 log.debug("DeploymentDataModel null when selecting models");
             }
@@ -73,20 +68,14 @@ public class ActivitiDAO {
     }
 
     /**
-     * invokes the DeploymentMapper.selectAllMetaData to retrieve all rows from BPS_BPMN_DEPLOYMENT_METADATA
+     * invokes the DeploymentMapper.selectAllMetaData to retrieve all rows from BPS_BPMN_DEPLOYMENT_METADATA.
      *
      * @return each row will be returned as DeploymentMetaDataModel with in list
      */
     public List<DeploymentMetaDataModel> selectAllDeploymentModels() {
 
         CustomSqlExecution<DeploymentMapper, List<DeploymentMetaDataModel>> customSqlExecution =
-                new AbstractCustomSqlExecution<DeploymentMapper, List<DeploymentMetaDataModel>>(
-                        DeploymentMapper.class) {
-                    public List<DeploymentMetaDataModel> execute(
-                            DeploymentMapper deploymentMapper) {
-                        return deploymentMapper.selectAllMetaData();
-                    }
-                };
+                new GetAllDeploymentMetaDataModelSqlExecution();
 
         return managementService.executeCustomSql(customSqlExecution);
     }
@@ -100,11 +89,7 @@ public class ActivitiDAO {
             final DeploymentMetaDataModel deploymentMetaDataModel) {
 
         CustomSqlExecution<DeploymentMapper, Integer> customSqlExecution =
-                new AbstractCustomSqlExecution<DeploymentMapper, Integer>(DeploymentMapper.class) {
-                    public Integer execute(DeploymentMapper deploymentMapper) {
-                        return deploymentMapper.insertDeploymentMetaData(deploymentMetaDataModel);
-                    }
-                };
+                new InsertDeploymentMetaDataModelSqlExecution(deploymentMetaDataModel);
 
         Integer rowCount = managementService.executeCustomSql(customSqlExecution);
 
@@ -123,11 +108,7 @@ public class ActivitiDAO {
             final DeploymentMetaDataModel deploymentMetaDataModel) {
 
         CustomSqlExecution<DeploymentMapper, Integer> customSqlExecution =
-                new AbstractCustomSqlExecution<DeploymentMapper, Integer>(DeploymentMapper.class) {
-                    public Integer execute(DeploymentMapper deploymentMapper) {
-                        return deploymentMapper.updateDeploymentMetaData(deploymentMetaDataModel);
-                    }
-                };
+                new UpdateDeploymentMetaDataModelSqlExecution(deploymentMetaDataModel);
 
         Integer rowCount = managementService.executeCustomSql(customSqlExecution);
 
@@ -146,11 +127,7 @@ public class ActivitiDAO {
             final DeploymentMetaDataModel deploymentMetaDataModel) {
 
         CustomSqlExecution<DeploymentMapper, Integer> customSqlExecution =
-                new AbstractCustomSqlExecution<DeploymentMapper, Integer>(DeploymentMapper.class) {
-                    public Integer execute(DeploymentMapper deploymentMapper) {
-                        return deploymentMapper.deleteDeploymentMetaData(deploymentMetaDataModel);
-                    }
-                };
+                new DeleteDeploymentMetaDataModelSqlExecution(deploymentMetaDataModel);
 
         Integer rowCount = managementService.executeCustomSql(customSqlExecution);
 
@@ -159,6 +136,79 @@ public class ActivitiDAO {
         }
 
         return rowCount;
+    }
+
+    static class GetDeploymentMetaDataModelSqlExecution extends AbstractCustomSqlExecution<DeploymentMapper, DeploymentMetaDataModel> {
+
+        String bpmnPackageName;
+
+        GetDeploymentMetaDataModelSqlExecution(String packageName) {
+            super(DeploymentMapper.class);
+            bpmnPackageName = packageName;
+        }
+
+        public DeploymentMetaDataModel execute(DeploymentMapper deploymentMapper) {
+            return deploymentMapper.selectMetaData(bpmnPackageName);
+        }
+
+    }
+
+    static class GetAllDeploymentMetaDataModelSqlExecution
+            extends AbstractCustomSqlExecution<DeploymentMapper, List<DeploymentMetaDataModel>> {
+
+        GetAllDeploymentMetaDataModelSqlExecution() {
+            super(DeploymentMapper.class);
+        }
+
+        public List<DeploymentMetaDataModel> execute(DeploymentMapper deploymentMapper) {
+            return deploymentMapper.selectAllMetaData();
+        }
+
+    }
+
+    static class InsertDeploymentMetaDataModelSqlExecution extends AbstractCustomSqlExecution<DeploymentMapper, Integer> {
+
+        DeploymentMetaDataModel deploymentMetaDataModel;
+
+        InsertDeploymentMetaDataModelSqlExecution(DeploymentMetaDataModel model) {
+            super(DeploymentMapper.class);
+            deploymentMetaDataModel = model;
+        }
+
+        public Integer execute(DeploymentMapper deploymentMapper) {
+            return deploymentMapper.insertDeploymentMetaData(deploymentMetaDataModel);
+        }
+
+    }
+
+    static class UpdateDeploymentMetaDataModelSqlExecution extends AbstractCustomSqlExecution<DeploymentMapper, Integer> {
+
+        DeploymentMetaDataModel deploymentMetaDataModel;
+
+        UpdateDeploymentMetaDataModelSqlExecution(DeploymentMetaDataModel model) {
+            super(DeploymentMapper.class);
+            deploymentMetaDataModel = model;
+        }
+
+        public Integer execute(DeploymentMapper deploymentMapper) {
+            return deploymentMapper.updateDeploymentMetaData(deploymentMetaDataModel);
+        }
+
+    }
+
+    static class DeleteDeploymentMetaDataModelSqlExecution extends AbstractCustomSqlExecution<DeploymentMapper, Integer> {
+
+        DeploymentMetaDataModel deploymentMetaDataModel;
+
+        DeleteDeploymentMetaDataModelSqlExecution(DeploymentMetaDataModel model) {
+            super(DeploymentMapper.class);
+            deploymentMetaDataModel = model;
+        }
+
+        public Integer execute(DeploymentMapper deploymentMapper) {
+            return deploymentMapper.deleteDeploymentMetaData(deploymentMetaDataModel);
+        }
+
     }
 
 }
