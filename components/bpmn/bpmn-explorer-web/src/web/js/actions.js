9a1dc9f7655cbc8b93e5ee88d54dc72c4ef34db1
Subasinghe <suba.11@cse.mrt.ac.lk>
2016-07-07 20:19:52 +0530

    https://wso2.org/jira/browse/BPS-1072

--- components/bpmn/bpmn-explorer-web/src/web/js/actions.js
+++ components/bpmn/bpmn-explorer-web/src/web/js/actions.js
@@ -464,7 +464,7 @@ function setDatePicker(dateElement) {
         singleDatePicker: true,
         showDropdowns: true,
         locale: {
-            format: 'MM/DD/YYYY'
+            format: 'YYYY-MM-DD'
         }
     });
 }
@@ -1430,7 +1430,6 @@ function validateFilter() {
         //disable startDateISO since it's not entered by the user
         document.getElementById("endDateISO").disabled = true;
     }
-
 }
 /**
  * Function to filter the records for the instance id in advanced filtering
@@ -1438,5 +1437,56 @@ function validateFilter() {
 function filterResults(id) {
 
     window.location = httpUrl + "/" + CONTEXT + "/processMonitoring?instanceId=" + id;
+}
+
+/**
+* Script to add more variable names and values to filter for instances
+*/
+function addVariable(name,value){
+    var vNames = document.getElementsByName("variableName");
+    if (vNames[vNames.length - 1].value !== "") {
+        var vRow = document.getElementById("variablesRow");
+
+        var vBr1 = document.createElement("BR");
+        vRow.appendChild(vBr1);
+
+        var vNameNode = document.createElement("INPUT");
+        vNameNode.setAttribute("type", "text");
+        vNameNode.setAttribute("name", "variableName");
+        vNameNode.setAttribute("class", "form-control");
+        vNameNode.setAttribute("placeholder", "Variable Name");
+        vNameNode.setAttribute("style", "width: initial; float: left;");
+        if(name != undefined) {
+          vNameNode.setAttribute("value", name);
+        }
+        vRow.appendChild(vNameNode);
+
+        var vLabel = document.createElement("LABEL");
+        var t = document.createTextNode("\u00A0=\u00A0");
+        vLabel.setAttribute("for", "equals");
+        vLabel.setAttribute("name", "variableName");
+        vLabel.setAttribute("class", "control-label");
+        vLabel.setAttribute("style", "width: initial; float: left; font-size:25px;");
+        vLabel.appendChild(t);
+        vRow.appendChild(vLabel);
+
+        var vValueNode = document.createElement("INPUT");
+        vValueNode.setAttribute("type", "text");
+        vValueNode.setAttribute("name", "variableValue");
+        vValueNode.setAttribute("class", "form-control");
+        vValueNode.setAttribute("placeholder", "Variable value like");
+        vValueNode.setAttribute("style", "width: initial;");
+        if(name != undefined) {
+            vNameNode.setAttribute("value", value);
+        }
+        vRow.appendChild(vValueNode);
+        var vBr = document.createElement("BR");
+        vRow.appendChild(vBr);
+    }
+}
+
+function resetForm() {
+    window.location = httpUrl + "/" + CONTEXT + "/advancedFilter";
+
 }
 
