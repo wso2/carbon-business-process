14f8889da9d0397a0a3b63a5fa28539cb413da92
Subasinghe <suba.11@cse.mrt.ac.lk>
2016-07-20 13:13:57 +0530

    change dynamic form generation with data required field

--- components/bpmn/bpmn-explorer-web/src/web/assets/dynamicFormGen.js
+++ components/bpmn/bpmn-explorer-web/src/web/assets/dynamicFormGen.js
@@ -28,12 +28,23 @@ function genSelect(data, disabled) {
             content += "<option value=\"" + data.enumValues[i].id + "\" "+selected +">" + data.enumValues[i].name + "</option>"
         }
     } else {
-         content += "<select name=\"" + data.id + "\" class=\"form-control\">";
-         for (var i = 0; i < data.enumValues.length; i++) {
-            if (data.value == data.enumValues[i].name) {
-             selected = 'selected';
-            }
-            content += "<option value=\"" + data.enumValues[i].id + "\" "+selected +">" + data.enumValues[i].name + "</option>"
+         if(data.required == true) {
+             content += "<select name=\"" + data.id + "\" class=\"form-control\" required>";
+             for (var i = 0; i < data.enumValues.length; i++) {
+                if (data.value == data.enumValues[i].name) {
+                 selected = 'selected';
+                }
+                content += "<option value=\"" + data.enumValues[i].id + "\" "+selected +">" + data.enumValues[i].name + "</option>"
+             }
+         }
+         else {
+              content += "<select name=\"" + data.id + "\" class=\"form-control\">";
+              for (var i = 0; i < data.enumValues.length; i++) {
+                 if (data.value == data.enumValues[i].name) {
+                  selected = 'selected';
+                 }
+                 content += "<option value=\"" + data.enumValues[i].id + "\" "+selected +">" + data.enumValues[i].name + "</option>"
+              }
          }
     }
     content += "</select></td></tr>";
@@ -51,8 +62,12 @@ function genCheckbox(data, disabled) {
     if (disabled == true || data.writable == "false") {
          content += "<input value=\"true\" name=\"" + data.id + "\" type=\"checkbox\" disabled=\"true\" "+checked +"/> " + data.name;
     } else {
-         content += "<input value=\"true\" name=\"" + data.id + "\" type=\"checkbox\" "+checked +"/> " + data.name;
-    }
+         if(data.required == true) {
+             content += "<input value=\"true\" name=\"" + data.id + "\"  type=\"checkbox\" "+checked +" required/> " + data.name;
+         }
+         else {
+             content += "<input value=\"true\" name=\"" + data.id + "\"  type=\"checkbox\" "+checked +" /> " + data.name;
+         }
     content += "</td></tr>";
     return content;
 }
@@ -66,7 +81,12 @@ function genCheckboxWithValues(data, disabled) {
     if (disabled == true || data.writable == "false") {
          content += "<input value=\"true\" name=\"" + data.id + "\" type=\"checkbox\" disabled=\"true\" "+checked +"/> " + data.name;
     } else {
-         content += "<input value=\"true\" name=\"" + data.id + "\"  type=\"checkbox\" "+checked +"/> " + data.name;
+        if(data.required == true) {
+            content += "<input value=\"true\" name=\"" + data.id + "\"  type=\"checkbox\" "+checked +" required/> " + data.name;
+        }
+        else {
+            content += "<input value=\"true\" name=\"" + data.id + "\"  type=\"checkbox\" "+checked +" /> " + data.name;
+        }
     }
     content += "</td></tr>";
     return content;
@@ -80,7 +100,12 @@ function genTextBox(data,disabled) {
     if (disabled == true || data.writable == false) {
         content += "<input name=\"" + data.id + "\" type=\"text\"  class=\"form-control\" disabled=\"true\"/>";
     } else {
-        content += "<input name=\"" + data.id + "\" type=\"text\"  class=\"form-control\"/>";
+        if(data.required == true) {
+            content += "<input name=\"" + data.id + "\" type=\"text\"  class=\"form-control\" required/>";
+        }
+        else {
+            content += "<input name=\"" + data.id + "\" type=\"text\"  class=\"form-control\"/>";
+        }
     }
     content += "</div></div>";
     return content;
@@ -93,7 +118,12 @@ function genTextBoxWithValues(data,disabled) {
     if (disabled == true || data.writable == false) {
         content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"text\" class=\"form-control\" disabled=\"true\"/>";
     } else {
-        content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"text\" class=\"form-control\" />";
+        if(data.required == true) {
+            content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"text\" class=\"form-control\" required/>";
+        }
+        else {
+            content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"text\" class=\"form-control\" />";
+        }
     }
     content += "</td></tr>";
     return content;
@@ -107,7 +137,12 @@ function genNumberBox(data, disabled) {
     if (disabled == true || data.writable == false) {
         content += "<input name=\"" + data.id + "\"  type=\"number\"  class=\"form-control\" disabled=\"true\"/>";
     } else {
-       content += "<input name=\"" + data.id + "\"  type=\"number\"  class=\"form-control\"/>";
+        if(data.required == true) {
+           content += "<input name=\"" + data.id + "\"  type=\"number\"  class=\"form-control\" required/>";
+        }
+        else {
+            content += "<input name=\"" + data.id + "\"  type=\"number\"  class=\"form-control\"/>";
+        }
     }
     content += "</div></div>";
     return content;
@@ -120,7 +155,12 @@ function genNumberBoxWithValues(data, disabled) {
     if (disabled == true || data.writable == false) {
         content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"number\"  class=\"form-control\" disabled=\"true\"/>";
     } else {
-        content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"number\"  class=\"form-control\"/>";
+        if(data.required == true) {
+            content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"number\"  class=\"form-control\" required/>";
+        }
+        else {
+            content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"number\"  class=\"form-control\"/>";
+        }
     }
     content += "</td></tr>";
     return content;
@@ -134,7 +174,12 @@ function genDatepicker(data, disabled) {
     if (disabled == true || data.writable == false) {
          content += "<input name=\"" + data.id + "\" type=\"date\"   class=\"form-control\" disabled=\"true\"/>";
     } else {
-         content += "<input name=\"" + data.id + "\" type=\"date\"   class=\"form-control\"/>";
+        if(data.required == true) {
+            content += "<input name=\"" + data.id + "\" type=\"date\"   class=\"form-control\" required/>";
+        }
+        else {
+            content += "<input name=\"" + data.id + "\" type=\"date\"   class=\"form-control\"/>";
+        }
     }
     content += "</td></tr>";
     return content;
@@ -147,7 +192,12 @@ function genDatepickerWithValues(data, disabled) {
     if (disabled == true || data.writable == false) {
           content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"date\"   class=\"form-control\" disabled=\"true\"/>";
     } else {
-          content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"date\"   class=\"form-control\"/>";
+        if(data.required == true) {
+            content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"date\"   class=\"form-control\" required/>";
+        }
+        else {
+            content += "<input name=\"" + data.id + "\" value=\"" + data.value + "\" type=\"date\"   class=\"form-control\"/>";
+        }
     }
     content += "</td></tr>";
     return content;
