71ee4c9c23184892c8e672167fa0322bf95da30e
drifftr <drifftr@gmail.com>
2017-12-20 19:02:19 +0530

    Add review suggestions

--- components/bpel/org.wso2.carbon.bpel/src/main/java/org/wso2/carbon/bpel/core/ode/integration/mgt/services/InstanceManagementServiceSkeleton.java
+++ components/bpel/org.wso2.carbon.bpel/src/main/java/org/wso2/carbon/bpel/core/ode/integration/mgt/services/InstanceManagementServiceSkeleton.java
@@ -1458,12 +1458,10 @@ public class InstanceManagementServiceSkeleton extends AbstractAdmin
             ProcessingException, InstanceManagementException {
         QName processId = getProcess(iid);
         TenantProcessStoreImpl processStore = getTenantProcessForCurrentSession();
-        if (processId != null) {
-            if (!processStore.containsProcess(processId)) {
-                log.error("Trying to invoke a illegal operation. Instance ID:" + iid);
-                throw new IllegalAccessException("Operation is not permitted.");
-            }
-        } else {
+        if (processId != null && !processStore.containsProcess(processId)) {
+            log.error("Trying to invoke a illegal operation. Instance ID:" + iid);
+            throw new IllegalAccessException("Operation is not permitted.");
+        } else if (processId == null) {
             throw new InstanceManagementException("Process instance for instance ID " + iid + " does not exist");
         }
     }
